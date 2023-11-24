34e942a6f58d678fd80c8aa5f13246ff6507f223
yairhdz24 <yairhdz107@gmail.com>
2023-11-24 02:55:51 -0600

    Implementacioin de squeleton

---
+++ Alitas_SBD23-main/src/components/shared/Squeleton.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+
+export const Squeleton = () => {
+    return (
+
+        
+        <section class="bg-white">
+            <div class="container px-6 py-10 mx-auto animate-pulse">
+                <h1 class="w-48 h-2 mx-auto bg-gray-200 rounded-lg"></h1>
+
+                <p class="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg"></p>
+                <p class="w-64 h-2 mx-auto mt-4 bg-gray-200 rounded-lg sm:w-80"></p>
+
+                <div class="grid grid-cols-1 gap-8 mt-8 xl:mt-12 xl:gap-12 sm:grid-cols-2 xl:grid-cols-4 lg:grid-cols-3">
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+
+                    <div class="w-full ">
+                        <div class="w-full h-64 bg-gray-300 rounded-lg "></div>
+
+                        <h1 class="w-56 h-2 mt-4 bg-gray-200 rounded-lg"></h1>
+                        <p class="w-24 h-2 mt-4 bg-gray-200 rounded-lg"></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+
+
+
+
+
+);
+} 
+{/* <h2 className="text-3xl font-Lilita_One text-alitas_obs_red">Error al cargar productos. Por favor, inténtalo de nuevo más tarde.</h2> */}
