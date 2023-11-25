c4b9c9bc5f43748e2fd18d208d100ca64c527dbb
yairhdz24 <yairhdz107@gmail.com>
2023-11-25 00:20:29 -0600

    ruta - clientes

--- Alitas_SBD23-main/src/main.jsx
+++ Alitas_SBD23-main/src/main.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Pedidos  from './pages/Pedidos';
+import Clientes from './pages/Clientes';
 
 import './index.css';
 
@@ -11,9 +12,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/pedidos" element={<Pedidos />} />
-        <Route path="/" element={<HomePage />} />
-        <Route path="/" element={<HomePage />} />
+        <Route path="/clientes" element={<Clientes />} />
       </Routes>
     </Router>
   </React.StrictMode>,
