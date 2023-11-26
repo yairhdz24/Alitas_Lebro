ff1cd5dffff905fd528d4ee3fcc7a04a513e4fbc
yairhdz24 <yairhdz107@gmail.com>
2023-11-26 01:31:39 -0600

    Modificacion de promesas

--- Backend-Alitas/controllers/pedidosController.js
+++ Backend-Alitas/controllers/pedidosController.js
@@ -1,80 +1,88 @@
-const pool = require('../db/db');
+const pool = require("../db/db");
 
 const obtenerPedidos = async (req, res) => {
   try {
     // Obtener pedidos desde la base de datos
-    const { rows } = await pool.query('SELECT * FROM pedidos');
+    const { rows } = await pool.query("SELECT * FROM pedidos");
 
     // Ejemplo de respuesta, ajusta según tus necesidades
     res.status(200).json(rows);
   } catch (error) {
-    console.error('Error al obtener pedidos:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
+    console.error("Error al obtener pedidos:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
+  }
+};
+
+const obtenerPedidoPorId = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const results = await pool.query(
+      "SELECT * FROM pedidos WHERE id_pedido = $1",
+      [id]
+    );
+
+    if (results.rows.length === 0) {
+      return res.status(404).json({ error: "Pedido no encontrado" });
+    }
+
+    res.json(results.rows[0]);
+  } catch (error) {
+    console.error("Error al obtener pedido por ID:", error.message);
+    res.status(500).send("Error interno del servidor");
   }
 };
 
 const crearPedido = async (req, res) => {
-  const { idCliente, nombre_cliente, productos, total } = req.body;
+  const { idCliente } = req.body;
 
   const client = await pool.connect();
 
   try {
-    await client.query('BEGIN');
+    await client.query("BEGIN");
 
     // Insertar un nuevo pedido
     const nuevoPedido = await client.query(
-      'INSERT INTO pedidos (id_cliente, fecha, estado) VALUES ($1, CURRENT_TIMESTAMP, $2) RETURNING id_pedido',
-      [idCliente, 'Pendiente']
+      "INSERT INTO pedidos (id_cliente, fecha, estado) VALUES ($1, CURRENT_TIMESTAMP, $2) RETURNING id_pedido",
+      [idCliente, "Pendiente"]
     );
 
     const idPedido = nuevoPedido.rows[0].id_pedido;
 
     // Realizar la insercion en el historial_pedidos a través de la funcion de trigger
 
-    await client.query('COMMIT');
+    await client.query("COMMIT");
 
-    res.status(201).json({ mensaje: 'Pedido creado exitosamente', idPedido });
+    res.status(201).json({ mensaje: "Pedido creado exitosamente", idPedido });
   } catch (error) {
-    await client.query('ROLLBACK');
-    console.error('Error al crear el pedido:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
+    await client.query("ROLLBACK");
+    console.error("Error al crear el pedido:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   } finally {
     client.release();
   }
 };
-
 
 const modificarPedido = async (req, res) => {
-  const { idPedido } = req.params;
+  const { id } = req.params;
   const { nuevoEstado } = req.body;
 
-  const client = await pool.connect();
-
   try {
-    await client.query('BEGIN');
-
-    // Modificar el estado del pedido
-    const result = await client.query(
-      'UPDATE pedidos SET estado = $1 WHERE id_pedido = $2 RETURNING *',
-      [nuevoEstado, idPedido]
+    const result = await pool.query(
+      "UPDATE pedidos SET estado = $1 WHERE id_pedido = $2 RETURNING *",
+      [nuevoEstado, id]
     );
 
-    
-    await client.query('COMMIT');
-
-    if (result.rows.length > 0) {
-      res.status(200).json({ mensaje: 'Pedido modificado exitosamente' });
-    } else {
-      res.status(404).json({ error: 'Pedido no encontrado' });
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Pedido no encontrado" });
     }
+
+    res.status(200).json({ mensaje: "Pedido modificado exitosamente" });
   } catch (error) {
-    await client.query('ROLLBACK');
-    console.error('Error al modificar el pedido:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
-  } finally {
-    client.release();
+    console.error("Error al modificar el pedido:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   }
 };
+
 
 const eliminarPedido = async (req, res) => {
   const { idPedido } = req.params;
@@ -82,20 +90,17 @@ const eliminarPedido = async (req, res) => {
   const client = await pool.connect();
 
   try {
-    await client.query('BEGIN');
+    await client.query("BEGIN");
 
     // Eliminar el pedido
-    await client.query(
-      'DELETE FROM pedidos WHERE id_pedido = $1',
-      [idPedido]
-    );
+    await client.query("DELETE FROM pedidos WHERE id_pedido = $1", [idPedido]);
 
-    await client.query('COMMIT');
-    res.status(200).json({ mensaje: 'Pedido eliminado exitosamente' });
+    await client.query("COMMIT");
+    res.status(200).json({ mensaje: "Pedido eliminado exitosamente" });
   } catch (error) {
-    await client.query('ROLLBACK');
-    console.error('Error al eliminar el pedido:', error);
-    res.status(500).json({ error: 'Error interno del servidor' });
+    await client.query("ROLLBACK");
+    console.error("Error al eliminar el pedido:", error);
+    res.status(500).json({ error: "Error interno del servidor" });
   } finally {
     client.release();
   }
@@ -103,7 +108,8 @@ const eliminarPedido = async (req, res) => {
 
 module.exports = {
   obtenerPedidos,
+  obtenerPedidoPorId,
   crearPedido,
   modificarPedido,
-  eliminarPedido
+  eliminarPedido,
 };
