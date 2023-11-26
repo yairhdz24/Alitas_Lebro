7a1e4e7108d4ce47b9fb60fcc10297df52645b43
yairhdz24 <yairhdz107@gmail.com>
2023-11-26 01:31:58 -0600

    Tabla modificada

--- Alitas_SBD23-main/src/pages/Pedidos.jsx
+++ Alitas_SBD23-main/src/pages/Pedidos.jsx
@@ -1,58 +1,75 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from '../components/shared/Sidebar';
-import { RiCloseLine, RiAddFill, RiFileList3Fill, RiMenu3Fill, RiDeleteBin6Fill, RiPencilFill } from 'react-icons/ri';
+import { RiCloseLine, RiAddFill, RiFileList3Fill, RiDeleteBin6Fill, RiPencilFill, RiMenu3Fill } from 'react-icons/ri';
 
 const Pedidos = () => {
   const [showMenu, setShowMenu] = useState(false);
-
-  const menu = () => {
-    setShowMenu(!showMenu);
-    // Otras lógicas necesarias al hacer clic en el botón del menu
-  };
-
-  // Pedidos de prueba
-  const [pedidos, setPedidos] = useState([
-    {
-      id: 1,
-      factura: true,
-      fecha: '2023-01-01',
-      estado: 'En Proceso',
-      cliente: 'Cliente de Prueba 1',
-      compra: '$50.00',
-    },
-    {
-      id: 2,
-      factura: false,
-      fecha: '2023-01-02',
-      estado: 'Entregado',
-      cliente: 'Cliente de Prueba 2',
-      compra: '$75.00',
-    },
-    {
-      id: 3,
-      factura: true,
-      fecha: '2023-01-03',
-      estado: 'Enviado',
-      cliente: 'Cliente de Prueba 3',
-      compra: '$30.00',
-    },
-    // Puedes agregar más pedidos según sea necesario
-  ]);
-
-  const handleEliminarPedido = (id) => {
-    const nuevosPedidos = pedidos.filter((pedido) => pedido.id !== id);
-    setPedidos(nuevosPedidos);
-  };
+  const [pedidos, setPedidos] = useState([]);
+  const [selectedPedidoId, setSelectedPedidoId] = useState(null);
+  const [selectedPedidoEstado, setSelectedPedidoEstado] = useState('');
 
   const handleEditarPedido = (id) => {
-    // Aquí puedes implementar la lógica para la edición del pedido
-    console.log('Editar pedido', id);
+    console.log("Pedido ID seleccionado:", id);
+    setSelectedPedidoId(id);
+    setShowMenu(true);
+  };
+
+  const handleModificarPedido = async () => {
+    try {
+      console.log('Pedido ID seleccionado:', selectedPedidoId);
+      console.log('Nuevo Estado:', selectedPedidoEstado);
+
+      const response = await fetch(`http://localhost:3001/pedidos/${selectedPedidoId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ nuevoEstado: selectedPedidoEstado }),
+      });
+
+      if (response.ok) {
+        console.log('Pedido modificado exitosamente');
+        // Actualizar el estado local
+        const nuevosPedidos = [...pedidos]; // Copiar el estado actual
+        const pedidoModificadoIndex = nuevosPedidos.findIndex(pedido => pedido.id_pedido === selectedPedidoId);
+        nuevosPedidos[pedidoModificadoIndex].estado = selectedPedidoEstado;
+        setPedidos(nuevosPedidos); // Actualizar el estado local
+      } else {
+        console.error('Error al modificar el pedido:', response.statusText);
+      }
+    } catch (error) {
+      console.error('Error al conectar con el servidor:', error);
+    }
+
+    setShowMenu(false);
+  };
+
+  useEffect(() => {
+    // Obtener pedidos desde el backend al cargar el componente
+    fetch('http://localhost:3001/pedidos')
+      .then((response) => response.json())
+      .then((data) => setPedidos(data))
+      .catch((error) => console.error('Error al obtener pedidos:', error));
+  }, []);
+
+  const handleEliminarPedido = (id) => {
+    // Eliminar pedido desde el backend
+    fetch(`http://localhost:3001/pedidos/${id}`, { method: 'DELETE' })
+      .then((response) => {
+        if (response.status === 200) {
+          // Actualizar la lista de pedidos después de la eliminación
+          setPedidos(pedidos.filter((pedido) => pedido.id_pedido !== id));
+        } else {
+          console.error('Error al eliminar pedido:', response.statusText);
+        }
+      })
+      .catch((error) => console.error('Error al eliminar pedido:', error));
   };
 
   return (
     <div className="bg-alitas_obs_beige w-full min-h-screen">
+      {/* Sidebar y Navbar */}
       <Sidebar showMenu={showMenu} />
-      {/* NAV de móvil */}
       <nav className="bg-alitas_beige lg:hidden fixed w-full bottom-0 left-0 text-3xl text-alitas_obs_red p-4 flex items-center justify-between rounded-tl-xl rounded-tr-xl">
         <button className="p-2">
           <RiCloseLine />
@@ -60,82 +77,85 @@ const Pedidos = () => {
         <button className="p-2">
           <RiAddFill />
         </button>
-        <button onClick={menu} className="p-2">
+        <button onClick={() => setShowMenu(!showMenu)} className="p-2">
           <RiFileList3Fill />
         </button>
-        <button onClick={menu} className="p-2">
+        <button onClick={() => setShowMenu(!showMenu)} className="p-2">
           {showMenu ? <RiCloseLine /> : <RiMenu3Fill />}
         </button>
       </nav>
+
       {/* Main */}
       <main className="lg:pl-32 lg:pr-96 pb-20">
         <div className="md:p-8 p-4">
-          <h1>CREA UNA TABLA DE PEDIDOS Pedidos</h1>
-          {/* Contenido adicional del componente de pedidos */}
-
+         
           {/* Tabla de pedidos */}
           <section className="container px-4 mx-auto">
             <div className="flex flex-col">
               <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-                  <div className="overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg">
-                    <table className="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
-                      <thead className="bg-gray-50 dark:bg-gray-800">
+                  <div className="overflow-hidden border border-gray-200 md:rounded-lg">
+                    <table className="min-w-full divide-y divide-gray-200">
+                      <thead className="bg-gray-50">
                         <tr>
-                          {/* ... (encabezados de la tabla) */}
+                          <th className="px-4 py-2 text-left">ID Pedido</th>
+                          <th className="px-4 py-2 text-left">Fecha</th>
+                          <th className="px-4 py-2 text-left">Estado</th>
+                          <th className="px-4 py-2 text-left">Cliente</th>
+                          <th className="px-4 py-2 text-left">Compra</th>
+                          <th className="px-4 py-2 text-left">Acciones</th>
                         </tr>
                       </thead>
-                      <tbody className="bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900">
+                      <tbody className="bg-white divide-y divide-gray-200">
                         {/* Filas de la tabla */}
                         {pedidos.map((pedido) => (
-                          <tr key={pedido.id}>
+                          <tr key={pedido.id_pedido}>
+                            <td className="px-4 py-4 whitespace-nowrap">#00{pedido.id_pedido}</td>
+                            <td className="px-4 py-4 whitespace-nowrap">{pedido.fecha}</td>
                             <td className="px-4 py-4 whitespace-nowrap">
-                              <div className="flex items-center gap-x-3">
-                                <input
-                                  type="checkbox"
-                                  className="text-blue-500 border-gray-300 rounded dark:bg-gray-900 dark:ring-offset-gray-900 dark:border-gray-700"
-                                />
-                                <button className="flex items-center gap-x-2">
-                                  <span>{pedido.factura ? 'Factura' : 'Sin factura'}</span>
-                                  <svg
-                                    className="h-3"
-                                    viewBox="0 0 10 11"
-                                    fill="none"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                  >
-                                    {/* ... (contenido del svg) */}
-                                  </svg>
-                                </button>
+                            <div className={`inline-flex items-center px-3 py-1 rounded-full ${pedido.estado === 'Entregado' ? 'bg-green-100/60' : (pedido.estado === 'Pendiente' ? 'bg-orange-100/60' : 'bg-red-100/60')}`}>
+                                <svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                  <circle cx="6" cy="6" r="5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" fill={`bg-${pedido.estado === 'Entregado' ? 'green' : (pedido.estado === 'Pendiente' ? 'orange' : 'red')}-500`} />
+                                </svg>
+                                <span className={`ml-2 text-${pedido.estado === 'Entregado' ? 'green' : (pedido.estado === 'Pendiente' ? 'orange' : 'red')}-500`}>{pedido.estado}</span>
                               </div>
+                              {showMenu && pedido.id_pedido === selectedPedidoId && (
+                                <div className="relative inline-block">
+                                  <button
+                                    onClick={() => setSelectedPedidoId(null)}
+                                    className="absolute inset-0 w-full h-full bg-transparent cursor-default"
+                                  ></button>
+                                  <div className="absolute -right-96 z-20 w-38 py-2 mt-2 origin-top-right bg-gray-200 rounded-md shadow-xl ">
+                                    <button
+                                      onClick={() => {
+                                        setSelectedPedidoEstado('Entregado');
+                                        handleModificarPedido();
+                                      }}
+                                      className="block px-4 py-3 text-sm text-gray-900 capitalize transition-colors duration-300 transform hover:bg-gray-100 "
+                                    >
+                                      Entregado
+                                    </button>
+                                    <button
+                                      onClick={() => {
+                                        setSelectedPedidoEstado('Cancelado');
+                                        handleModificarPedido();
+                                      }}
+                                      className="block px-4 py-3 text-sm text-gray-900 capitalize transition-colors duration-300 transform hover:bg-gray-100 "
+                                    >
+                                      Cancelado
+                                    </button>
+                                  </div>
+                                </div>
+                              )}
                             </td>
+                            <td className="px-4 py-4 whitespace-nowrap">{pedido.cliente}</td>
+                            <td className="px-4 py-4 whitespace-nowrap">{pedido.compra}</td>
                             <td className="px-4 py-4 whitespace-nowrap">
-                              <div className="text-sm text-gray-900">{pedido.fecha}</div>
-                            </td>
-                            <td className="px-4 py-4 whitespace-nowrap">
-                              <div className="text-sm text-gray-900">{pedido.estado}</div>
-                            </td>
-                            <td className="px-4 py-4 whitespace-nowrap">
-                              <div className="text-sm text-gray-900">{pedido.cliente}</div>
-                            </td>
-                            <td className="px-4 py-4 whitespace-nowrap">
-                              <div className="text-sm text-gray-900">{pedido.compra}</div>
-                            </td>
-                            <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                              {/* Botón de eliminar */}
-                              <button
-                                onClick={() => handleEliminarPedido(pedido.id)}
-                                className="text-red-600 hover:text-red-900"
-                                title="Eliminar"
-                              >
-                                <RiDeleteBin6Fill />
-                              </button>
-                              {/* Botón de editar */}
-                              <button
-                                onClick={() => handleEditarPedido(pedido.id)}
-                                className="text-blue-600 hover:text-blue-900 ml-2"
-                                title="Editar"
-                              >
+                              <button onClick={() => handleEditarPedido(pedido.id_pedido)} className="text-alitas_blue hover:underline">
                                 <RiPencilFill />
+                              </button>
+                              <button onClick={() => handleEliminarPedido(pedido.id_pedido)} className="text-red-500 hover:underline ml-2">
+                                <RiDeleteBin6Fill />
                               </button>
                             </td>
                           </tr>
@@ -145,40 +165,6 @@ const Pedidos = () => {
                   </div>
                 </div>
               </div>
-            </div>
-
-
-             {/* Paginación */}
-            <div className="flex items-center justify-between mt-6">
-              <a href="#" className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 15.75L3 12m0 0l3.75-3.75M3 12h18" />
-                </svg>
-
-                <span>
-                  Anterior
-                </span>
-              </a>
-
-              <div className="items-center hidden md:flex gap-x-3">
-                <a href="#" className="px-2 py-1 text-sm text-blue-500 rounded-md dark:bg-gray-800 bg-blue-100/60">1</a>
-                <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">2</a>
-                <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">3</a>
-                <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">...</a>
-                <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">12</a>
-                <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">13</a>
-                <a href="#" className="px-2 py-1 text-sm text-gray-500 rounded-md dark:hover:bg-gray-800 dark:text-gray-300 hover:bg-gray-100">14</a>
-              </div>
-
-              <a href="#" className="flex items-center px-5 py-2 text-sm text-gray-700 capitalize transition-colors duration-200 bg-white border rounded-md gap-x-2 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-200 dark:border-gray-700 dark:hover:bg-gray-800">
-                <span>
-                  Siguiente
-                </span>
-
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-5 h-5 rtl:-scale-x-100">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3" />
-                </svg>
-              </a>
             </div>
           </section>
         </div>
