6f12c386520aa5a69c2b118a5f8775e12e594f8e
yairhdz24 <yairhdz107@gmail.com>
2023-11-28 23:35:10 -0600

    s

--- Alitas_SBD23-main/vite.config.js
+++ Alitas_SBD23-main/vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '', // Deja esto como una cadena vacía si tu aplicación está en la raíz
   plugins: [react()],
-})
+});
